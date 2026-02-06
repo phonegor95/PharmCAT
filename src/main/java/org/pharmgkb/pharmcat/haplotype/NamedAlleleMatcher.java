4964ee71b113345524caa5fe217a864fc6f7ac4e
Mark Woon <markwoon@gmail.com>
2026-02-06 14:16:02 -0800

    feat(namedAlleleMatcher): support handling repeat wobbles (NUDT15)

--- src/main/java/org/pharmgkb/pharmcat/haplotype/NamedAlleleMatcher.java
+++ src/main/java/org/pharmgkb/pharmcat/haplotype/NamedAlleleMatcher.java
@@ -43,6 +43,7 @@ import static org.pharmgkb.pharmcat.Constants.isLowestFunctionGene;
 public class NamedAlleleMatcher {
   public static final String VERSION = "2.0.0";
   // CHANGES TO THIS LIST OF GENES SHOULD ALSO BE REFLECTED IN DOCUMENTATION IN NamedAlleleMatcher-101.md
+  /* Treat undocumented variations in these genes as reference due to drug toxicity. */
   public static final List<String> TREAT_UNDOCUMENTED_VARIATIONS_AS_REFERENCE = List.of(
       "CACNA1S",
       "DPYD",
